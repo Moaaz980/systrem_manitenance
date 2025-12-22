9e1ca8ce00f680c8dc4904634917bea5f954e3db
Moaaz980 <moaaz.bahnassawi@itsrizzoli.it>
2025-12-22 14:59:35 +0100

    struttura del progetto front-end e back-end

---
+++ back-end/manitenanceSystem/src/main/java/com/example/manitenanceSystem/ManitenanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.manitenanceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManitenanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManitenanceSystemApplication.class, args);
+	}
+
+}
