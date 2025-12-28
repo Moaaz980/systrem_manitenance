94f8e27eca3a9dbf2cc0d6a48d8413905d57e8d6
Moaaz980 <moaaz.bahnassawi@itsrizzoli.it>
2025-12-28 18:34:08 +0100

    installato angular material per componenti UI modificato pagine login e registrazione

--- front-end/manitenance_system_frontend/src/shared/register/register.component.ts
+++ front-end/manitenance_system_frontend/src/shared/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , inject } from '@angular/core';
+import { LogoApp } from '../../core/logo-app.service';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  titolo = inject(LogoApp);
 }
