eaa743d390c97e5c48895063e8e89152dfe82107
Moaaz980 <moaaz.bahnassawi@itsrizzoli.it>
2026-01-04 17:15:02 +0100

    validazione form login e register con i messaggi di errore

--- front-end/manitenance_system_frontend/src/shared/authform/authform.component.ts
+++ front-end/manitenance_system_frontend/src/shared/authform/authform.component.ts
@@ -2,12 +2,14 @@ import { Component , Input } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-authform',
   standalone: true ,
-  imports: [MatInputModule , MatFormFieldModule],
+  imports: [MatInputModule , MatFormFieldModule , FormsModule , CommonModule],
   templateUrl: './authform.component.html',
   styleUrl: './authform.component.css'
 })
@@ -18,4 +20,20 @@ export class AuthformComponent {
   goToHome():void {
     this.router.navigate(['/home']);
   }
+
+
+  username:string = '';
+  password:string = '';
+  role:string = '';
+  globalError:string = '';
+
+
+  onSubmit(form:NgForm) {
+    if (!form.valid) {
+      this.globalError = 'Compila tutti i campi';
+      setTimeout(() => {
+          this.globalError = '';
+      }, 2000);
+  }
+}
 }
